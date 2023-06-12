1bc8f55ce9fd3491519b78233e66c094db89e365
TaisVieira <63369712+TaisVieira@users.noreply.github.com>
2023-06-12 13:17:43 -0400

    Update scRNAProcessing.R

--- scRNAProcessing.R
+++ scRNAProcessing.R
@@ -31,8 +31,6 @@ rownames(mat) = feature.names$V1
 
 expr <- as.data.frame(as.matrix(mat))
 
-write.csv(expr, paste(input.dir, "pf_scrna_count_table.csv", sep=""))
-
 S.O.RNA<-CreateSeuratObject(counts = expr, min.cells = 10, min.features = 100)
 VlnPlot(S.O.RNA, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
 FeatureScatter(S.O.RNA, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
@@ -44,7 +42,7 @@ S.O.RNA <- prep_S.O(S.O.RNA)
 DimPlot(S.O.RNA, reduction = 'pca')
 DimPlot(object = S.O.RNA, label = TRUE, reduction = 'umap') + NoLegend()
 
-saveRDS(S.O.gam, '../S.O.exp.rds')
+saveRDS(S.O.RNA, '../S.O.exp.rds')
 
 # published data
 gam_file<-read.csv("../scRNAseq_Pf/Fig1-10X-data.csv", sep=";")
@@ -73,3 +71,33 @@ S.O.gam<-AddMetaData(S.O.gam, meta)
 Idents(S.O.gam)<-'Cell_Type'
 
 saveRDS(S.O.gam, '../S.O.pub.rds')
+
+# MCA data
+file.counts <- read.csv("../scRNAseq_Pf/MalariaCellAtlas-master/Expression_Matrices/10X/pf10xIDC/pf10xIDC_counts.csv")
+genes <- file.counts$X
+expr<-file.counts[,-1]
+rownames(expr) <- genes
+
+S.O.MCA<-CreateSeuratObject(counts = expr, min.cells = 10, min.features = 100)
+VlnPlot(S.O.MCA, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
+FeatureScatter(S.O.MCA, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+quant1<-quantile(S.O.MCA$nFeature_RNA, probs = c(0.1,0.98))
+quant2<-quantile(S.O.MCA$nCount_RNA, probs = c(0.1,0.98))
+S.O.MCA<-subset(S.O.MCA, subset = nFeature_RNA > quant1[1] & nFeature_RNA < quant1[2] & 
+                  nCount_RNA > quant2[1] & nCount_RNA < quant2[2])
+S.O.MCA <- prep_S.O(S.O.MCA)
+DimPlot(S.O.MCA, reduction = 'pca')
+DimPlot(object = S.O.MCA, label = TRUE, reduction = 'umap') + NoLegend()
+
+meta_data<-read.csv("../scRNAseq_Pf/MalariaCellAtlas-master/Expression_Matrices/10X/pf10xIDC/pf10xIDC_pheno.csv")
+cell_id <- meta_data$X
+meta_data <- meta_data[,-1]
+rownames(meta_data) <- cell_id
+meta_data<-meta_data[,c('sample_id', 'clock_pseudotime', 'bulk')]
+colnames(meta_data)<-c('sample_id', 'clock_pseudotime', 'stage')
+S.O.MCA <- AddMetaData(S.O.MCA, meta_data)
+Idents(S.O.MCA)<-'stage'
+S.O.MCA@meta.data$group <- 'idc'
+DimPlot(S.O.MCA, reduction = 'pca')
+
+saveRDS(S.O.gam, '../S.O.mca.rds')
