da25781fb8176b80e6fd8d930768adc510ee5924
TaisVieira <63369712+TaisVieira@users.noreply.github.com>
2023-06-12 15:27:37 -0400

    Update scRNAIntegration.R

--- scRNAIntegration.R
+++ scRNAIntegration.R
@@ -1,72 +1,111 @@
-################################################
-##   Integrates gametocyte data from scRNA    ##
-## experiment with published gametocyte data  ##
-################################################
+##########################################
+##  Integrates scRNA experimental data  ##
+##    with published gametocyte data    ##
+##     and Malaria Cell Atlas data      ##
+##########################################
 
 library(Seurat)
 library(ggplot2)
+library(tidyverse)
+library(plotly)
 
 source('../util_funcs.R')
 
-gam.exp<-readRDS("../S.O.exp")
-gam.pub<-readRDS("../S.O.pub")
+S.O.exp<-readRDS("../S.O.exp.rds")
+S.O.pub<-readRDS("../S.O.pub.rds")
+S.O.mca<-readRDS("../S.O.mca.rds")
 
-# integrate both objects
-S.O.list <- list(gam.exp = gam.exp, gam.pub = gam.pub)
-S.O.list <- lapply(S.O.list, FUN = function(x) {
-  DefaultAssay(x) <- 'RNA'
-  x <- NormalizeData(x)
-  x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
-})
+S.O.exp.idc<-subset(S.O.exp, ident=c('0','1','2','4','5'))
+S.O.exp.gam<-subset(S.O.exp, ident='3')
 
-features <- SelectIntegrationFeatures(object.list = S.O.list)
-spps <- names(S.O.list)
-reference_dataset <- 2
-anchors <- FindIntegrationAnchors(object.list = S.O.list, anchor.features = features, dims = 1:30)
-gam_integrated <- IntegrateData(anchorset = anchors, dims = 1:30, k.weight = 65)
+# integrate experimental data with MCA data
+S.O.mca$orig.ident<-'mca'
+S.O.exp.idc$orig.ident<-'exp'
+S.O.list <- list(S.O.exp.idc = S.O.exp.idc, S.O.mca = S.O.mca)
+idc_integrated<-integrate_S.O(S.O.list, 2)
+Idents(idc_integrated)<-'stage'
+DimPlot(idc_integrated, reduction = 'pca')
+DimPlot(object = idc_integrated, label = TRUE, reduction = 'umap') + NoLegend()
 
-# switch to integrated assay. Make sure to set to RNA for Differential Expression
-DefaultAssay(gam_integrated) <- "integrated"
-gam_integrated <- ScaleData(object = gam_integrated, verbose = FALSE)
-gam_integrated <- RunPCA(gam_integrated, features = VariableFeatures(object = gam_integrated))
-gam_integrated <- FindNeighbors(gam_integrated, dims = 1:10, reduction = 'pca')
-gam_integrated <- FindClusters(gam_integrated, resolution = 0.5)
-gam_integrated <- RunUMAP(gam_integrated, dims = 1:13)
+# transfer labels from MCA to experimental data
+Idents(idc_integrated) <- 'orig.ident'
+exp_sub <- subset(idc_integrated, ident = 'exp')
+mca_sub <- subset(idc_integrated, ident = 'mca')
+
+anchors <- FindTransferAnchors(reference = mca_sub, query = exp_sub, dims = 1:30)
+predictions <- TransferData(anchorset = anchors, refdata = mca_sub@meta.data$stage, dims = 1:30)
+exp_sub <- AddMetaData(object = exp_sub, metadata = predictions)
+exp_sub@meta.data$stage <- exp_sub@meta.data$predicted.id
+Idents(exp_sub) <- 'stage'
+DimPlot(exp_sub, reduction = 'pca')
+
+ind1 <- idc_integrated@meta.data$orig.ident == 'exp'
+ind2 <- match(rownames(idc_integrated@meta.data)[ind1], rownames(exp_sub@meta.data))
+idc_integrated@meta.data$stage[ind1] <- exp_sub@meta.data$stage[ind2]
+
+Idents(idc_integrated) <- 'stage'
+idc_integrated$stage <- factor(idc_integrated$stage, levels = c('late_troph', 'early_troph', 'schizont', 'ring'))
+idc_integrated@meta.data$stage <- factor(idc_integrated@meta.data$stage, 
+                                         levels = c('late_troph', 'early_troph', 'schizont', 'ring'))
+DimPlot(idc_integrated, reduction = 'pca')
+DimPlot(object = idc_integrated, label = TRUE, reduction = 'umap') + NoLegend()
+
+saveRDS(idc_integrated, "../S.O.integrated_idc.rds")
+
+##############################
+
+# integrate published data with experimental gametocyte data
+S.O.pub$orig.ident<-'pub'
+S.O.exp.gam$orig.ident<-'exp'
+S.O.list <- list(S.O.exp.gam = S.O.exp.gam, S.O.pub = S.O.pub)
+gam_integrated<-integrate_S.O(S.O.list, 2)
 Idents(gam_integrated)<-'Cell_Type'
 DimPlot(gam_integrated, reduction = 'pca')
 DimPlot(object = gam_integrated, label = TRUE, reduction = 'umap') + NoLegend()
 
 # transfer labels to experimental data
-gam_integrated@meta.data$orig.ident<-ifelse(gam_integrated@meta.data$orig.ident == "NF54", "pub", 'exp')
 Idents(gam_integrated) <- 'orig.ident'
-gam1_sub <- subset(gam_integrated, ident = 'exp')
-gam2_sub <- subset(gam_integrated, ident = 'pub')
+exp_sub <- subset(gam_integrated, ident = 'exp')
+pub_sub <- subset(gam_integrated, ident = 'pub')
 
-anchors <- FindTransferAnchors(reference = gam2_sub, query = gam1_sub, dims = 1:30)
-predictions <- TransferData(anchorset = anchors, refdata = gam2_sub@meta.data$Cell_Type,dims = 1:30)
-gam1_sub <- AddMetaData(object = gam1_sub, metadata = predictions)
-gam1_sub@meta.data$Cell_Type <- gam1_sub@meta.data$predicted.id
-Idents(gam1_sub) <- 'Cell_Type'
-DimPlot(gam1_sub, reduction = 'pca')
+anchors <- FindTransferAnchors(reference = pub_sub, query = exp_sub, dims = 1:30)
+predictions <- TransferData(anchorset = anchors, refdata = pub_sub@meta.data$Cell_Type, dims = 1:30)
+exp_sub <- AddMetaData(object = exp_sub, metadata = predictions)
+exp_sub@meta.data$Cell_Type <- exp_sub@meta.data$predicted.id
+Idents(exp_sub) <- 'Cell_Type'
+DimPlot(exp_sub, reduction = 'pca')
 
 ind1 <- gam_integrated@meta.data$orig.ident == 'exp'
-ind2 <- match(rownames(gam_integrated@meta.data)[ind1], rownames(gam1_sub@meta.data))
-gam_integrated@meta.data$Cell_Type[ind1] <- gam1_sub@meta.data$Cell_Type[ind2]
+ind2 <- match(rownames(gam_integrated@meta.data)[ind1], rownames(exp_sub@meta.data))
+gam_integrated@meta.data$Cell_Type[ind1] <- exp_sub@meta.data$Cell_Type[ind2]
 
-Idents(gam_integrated) <- 'Cell_Type'
-gam_integrated$Cell_Type <- factor(gam_integrated$Cell_Type, levels = c('progenitor', 'female', 'male'))
-gam_integrated@meta.data$Cell_Type <- factor(gam_integrated@meta.data$Cell_Type, levels = c('progenitor', 'female', 'male'))
-p <- DimPlot(gam_integrated, reduction = "pca", 
-             pt.size = 1,
-             label = T, label.size = 5) + NoLegend() + 
-  theme(panel.spacing = unit(0.5, "lines")) + 
-  theme(axis.text.x = element_text(face="bold", size=12, angle=0)) +
-  theme(axis.text.y = element_text(face="bold", size=12, angle=0)) +
-  theme(
-    axis.title.x = element_text(size=14, face="bold"),
-    axis.title.y = element_text(size=14, face="bold")
-  )
-
-plot(p)
+gam_integrated$stage<-gam_integrated$Cell_Type
+Idents(gam_integrated) <- 'stage'
+gam_integrated$stage <- factor(gam_integrated$stage, levels = c('progenitor', 'female', 'male'))
+gam_integrated@meta.data$stage <- factor(gam_integrated@meta.data$stage, 
+                                         levels = c('progenitor', 'female', 'male'))
+DimPlot(gam_integrated, reduction = 'pca')
+DimPlot(object = gam_integrated, label = TRUE, reduction = 'umap') + NoLegend()
 
 saveRDS(gam_integrated, "../S.O.integrated_gam.rds")
+
+##############################
+
+# integrate IDC object with gametocytes object
+idc_integrated$orig.ident<-'idc'
+gam_integrated$orig.ident<-'gam'
+S.O.list <- list(idc_integrated = idc_integrated, gam_integrated = gam_integrated)
+full_integrated<-integrate_S.O(S.O.list, 1)
+Idents(full_integrated)<-'stage'
+DimPlot(full_integrated, reduction = 'pca')
+
+S.O.intra <- RunUMAP(gam_integrated, dims = 1:13, n.components = 3)
+pca.full <- getPcaUmapMetaData(S.O.intra)
+
+fig <- plot_ly(pca.full, x = ~PC_1, y = ~PC_2, z = ~PC_3, color = ~stage, 
+               colors = c('#f8766d', '#a2a400', '#00bf7d', '#04b0f6', '#e76bf3')) 
+fig <- fig %>% add_markers(size = 2)
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'PC_1'),
+                                   yaxis = list(title = 'PC_2'),
+                                   zaxis = list(title = 'PC_3')))
+fig
