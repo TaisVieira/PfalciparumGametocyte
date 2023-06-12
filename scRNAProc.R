43f6ca5a70259cbc9dc6d2a03f607de371aaba87
TaisVieira <63369712+TaisVieira@users.noreply.github.com>
2023-06-12 11:10:56 -0400

    Update scRNAProc.R

--- scRNAProc.R
+++ scRNAProc.R
@@ -9,7 +9,8 @@ library(Matrix)
 
 source('../util_funcs.R')
 
-input.dir <-  "C:/Users/Tais.Vieira/Desktop/research/Pf/scRNAseq_Pf/outs/"
+# experiment data
+input.dir <-  "../scRNAseq_Pf/outs/"
 
 barcode.path <- paste(input.dir, "filtered_feature_bc_matrix/barcodes.tsv.gz", sep = "")
 features.path <- paste(input.dir, "filtered_feature_bc_matrix/features.tsv.gz", sep = "")
@@ -43,4 +44,34 @@ S.O.RNA <- prep_S.O(S.O.RNA)
 DimPlot(S.O.RNA, reduction = 'pca')
 DimPlot(object = S.O.RNA, label = TRUE, reduction = 'umap') + NoLegend()
 
-saveRDS(S.O.RNA, '../S.O.proc')
+S.O.gam<-subset(S.O.RNA, idents='3')
+
+saveRDS(S.O.gam, '../S.O.exp')
+
+# published data
+gam_file<-read.csv("../scRNAseq_Pf/Fig1-10X-data.csv", sep=";")
+
+gam_file<-t(gam_file)
+colnames(gam_file)<-gam_file[1,]
+gam_file<-as.data.frame(gam_file[-1,])
+
+counts<-gam_file[which(gam_file$Genotype == "WT"),5:ncol(gam_file)]
+meta<-gam_file[,1:4]
+
+expr<-t(counts)
+
+S.O.gam<-CreateSeuratObject(counts = expr, min.cells = 10, min.features = 100)
+VlnPlot(S.O.gam, features = c("nFeature_RNA", "nCount_RNA"), ncol = 2)
+FeatureScatter(S.O.gam, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+quant1<-quantile(S.O.gam$nFeature_RNA, probs = c(0.1,0.98))
+quant2<-quantile(S.O.gam$nCount_RNA, probs = c(0.1,0.98))
+S.O.gam<-subset(S.O.gam, subset = nFeature_RNA > quant1[1] & nFeature_RNA < quant1[2] & 
+                  nCount_RNA > quant2[1] & nCount_RNA < quant2[2])
+S.O.gam <- prep_S.O(S.O.gam)
+DimPlot(S.O.gam, reduction = 'pca')
+DimPlot(object = S.O.gam, label = TRUE, reduction = 'umap') + NoLegend()
+
+S.O.gam<-AddMetaData(S.O.gam, meta)
+Idents(S.O.gam)<-'Cell_Type'
+
+saveRDS(S.O.gam, '../S.O.pub')
