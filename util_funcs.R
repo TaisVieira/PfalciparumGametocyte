4cd2572525b4437c42b70889bf8d56a15fd2a273
TaisVieira <63369712+TaisVieira@users.noreply.github.com>
2023-06-12 14:24:42 -0400

    Update util_funcs.R

--- util_funcs.R
+++ util_funcs.R
@@ -19,3 +19,26 @@ prep_S.O <- function(S.O, res = 0.1, var.features = F, down.sample = F){
   S.O <- RunUMAP(S.O, dims = 1:13, n.components = 3L)
   return(S.O)
 }
+
+integrate_S.O<-function(S.O.list, ref.dataset){
+  set.seed(100)
+  S.O.list <- lapply(S.O.list, FUN = function(x) {
+    DefaultAssay(x) <- 'RNA'
+    x <- NormalizeData(x)
+    x <- FindVariableFeatures(x, selection.method = "vst", nfeatures = 3000)
+  })
+  
+  features <- SelectIntegrationFeatures(object.list = S.O.list)
+  spps <- names(S.O.list)
+  reference_dataset <- ref.dataset
+  anchors <- FindIntegrationAnchors(object.list = S.O.list, anchor.features = features, dims = 1:30)
+  S.O.integrated <- IntegrateData(anchorset = anchors, dims = 1:30, k.weight = 65)
+  
+  # switch to integrated assay. Make sure to set to RNA for Differential Expression
+  DefaultAssay(S.O.integrated) <- "integrated"
+  S.O.integrated <- ScaleData(object = S.O.integrated, verbose = FALSE)
+  S.O.integrated <- RunPCA(S.O.integrated, features = VariableFeatures(object = S.O.integrated))
+  S.O.integrated <- FindNeighbors(S.O.integrated, dims = 1:10, reduction = 'pca')
+  S.O.integrated <- FindClusters(S.O.integrated, resolution = 0.5)
+  S.O.integrated <- RunUMAP(S.O.integrated, dims = 1:13)
+}
